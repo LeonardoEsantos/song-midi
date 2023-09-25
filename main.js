17ddc5442dde8f27af6b1f13710dda24576ce523
Leonardo Santos <leonardo.esantos2@gmail.com>
2023-09-25 15:48:36 -0300

    Segunda aula

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(params) {
+    document.querySelector('#som_tecla_pom').play();    
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+function tocaSomClap(params) {
+    document.querySelector('#som_tecla_clap').play();    
+}
+
+function tocaSomTim(params) {
+    document.querySelector('#som_tecla_tim').play();    
+}
+
+function tocaSomPuff(params) {
+    document.querySelector('#som_tecla_puff').play();    
+}
+
+function tocaSomSplash(params) {
+    document.querySelector('#som_tecla_splash').play();    
+}
+
+function tocaSomToim(params) {
+    document.querySelector('#som_tecla_toim').play();    
+}
+
+function tocaSomPsh(params) {
+    document.querySelector('#som_tecla_psh').play();    
+}
+
+function tocaSomTic(params) {
+    document.querySelector('#som_tecla_tic').play();    
+}
+
+function tocaSomTom(params) {
+    document.querySelector('#som_tecla_tom').play();    
+}
+
