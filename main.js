335e4e32015d25f938b6a3943a665fe7b60e7a6f
Leonardo Santos <leonardo.esantos2@gmail.com>
2023-09-25 15:52:25 -0300

    Segunda aula com todos botões funcionando

--- main.js
+++ main.js
@@ -8,31 +8,46 @@ function tocaSomClap(params) {
     document.querySelector('#som_tecla_clap').play();    
 }
 
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
 function tocaSomTim(params) {
     document.querySelector('#som_tecla_tim').play();    
 }
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
 function tocaSomPuff(params) {
     document.querySelector('#som_tecla_puff').play();    
 }
 
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
 function tocaSomSplash(params) {
     document.querySelector('#som_tecla_splash').play();    
 }
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
 
 function tocaSomToim(params) {
     document.querySelector('#som_tecla_toim').play();    
 }
 
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
 function tocaSomPsh(params) {
     document.querySelector('#som_tecla_psh').play();    
 }
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
 
 function tocaSomTic(params) {
     document.querySelector('#som_tecla_tic').play();    
 }
 
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
 function tocaSomTom(params) {
     document.querySelector('#som_tecla_tom').play();    
 }
 
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
